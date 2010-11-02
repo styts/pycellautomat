9c6fa61206fc943345338579066cd38f064c50db
Kirill Stytsenko <kirstyts@gmail.com>
2010-11-02 19:24:33 +0100

    working on html output

--- src/main.py
+++ src/main.py
@@ -10,12 +10,16 @@ rule_numbers = range(20)
 #rule_numbers = range(256)
     
 
-"""Rules are the transformations of a 3-tuple of binary neighbours into a output bit. n defines the Wolfram Rule"""
-def get_rules(n):
+def d2b(n):
     bi = bin(n)
     bi = bi[2:]
     while len(bi)<8:
         bi = '0' + bi
+    return bi
+
+"""Rules are the transformations of a 3-tuple of binary neighbours into a output bit. n defines the Wolfram Rule"""
+def get_rules(n):
+    bi = d2b(n)
     rules = [# [input]      [out]
              ([1, 1, 1] , int(bi[0])),
              ([1, 1, 0] , int(bi[1])),
@@ -59,23 +63,40 @@ def bar(line,pix,rule_nr):
             c = line[j]
             pix[j,i] = (0,0,0) if c == 1 else (255,255,255)
 
-def main():    
-    for rn in rule_numbers:
-        # create image
-        im = Image.new("RGB", (w, h), "white")
-        pix = im.load() # pixel access matrix
-        
-        # init first line
-        line = list(numpy.zeros(w, dtype=numpy.int))
-        line[w/2] = 1
-        
-        bar(line,pix,rn)
+def out_html(rule_numbers):
+    fp = open("../output/main.html",'w')
+    s = "<html><body>\n"
+    for r in rule_numbers:
+        s += "\t<span class='bla'>"
+        s += "\t\t<span class='dec'>Rule %s</span>" % r
+        s += "\t\t<pre class='bi' >%s</pre>" % d2b(r)
+        s += "\t\t<img src='images/rule%s.png'>\n" % r
+        s += "\t</span>"
+    s += "</body></html>\n"
+    fp.write(s)
+    fp.close()
+    print "output/main.html generated"
     
-        im = im.resize((w*pixel_factor,h*pixel_factor), Image.NEAREST )
-        if not os.path.exists("../output"):
-            os.makedirs("../output")
-        im.save("../output/%s.png" % rn, "PNG")
+def main():
+    if not os.path.isfile("../output/images/rule%s.png" % rule_numbers[len(rule_numbers)-1]): # last file exists -> don't generate
+        for rn in rule_numbers:
+            # create image
+            im = Image.new("RGB", (w, h), "white")
+            pix = im.load() # pixel access matrix
+            
+            # init first line
+            line = list(numpy.zeros(w, dtype=numpy.int))
+            line[w/2] = 1
+            
+            bar(line,pix,rn)
         
-        print "Rule %s" % rn
+            im = im.resize((w*pixel_factor,h*pixel_factor), Image.NEAREST )
+            if not os.path.exists("../output/images"):
+                os.makedirs("../output/images")
+            im.save("../output/images/rule%s.png" % rn, "PNG")
+            
+            print "Rule %s" % rn
+        
+    out_html(rule_numbers)
     
 main()
